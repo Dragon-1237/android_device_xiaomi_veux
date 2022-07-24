c4f36df0bad08d8eb4c606dc6382cd64a89a08b5
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-07-24 20:45:19 +0530

    Update and rename lineage_veux.mk to corvus_veux.mk

--- corvus_veux.mk
+++ corvus_veux.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,19 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common corvus stuff.
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-PRODUCT_NAME := lineage_veux
+PRODUCT_NAME := corvus_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X4 Pro 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+CORVUS_MAINTAINER=Anant_Goel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="veux-user 11 RKQ1.211001.001 V13.0.10.0.RKCMIXM release-keys"
